695eef42604ef0ae6a98b3e081388ab4fbfdeee7
ThiagoSiqueira <thiago.s.catharino@gmail.com>
2025-06-06 14:40:17 -0300

    Update models.py
    Remontando o modelo Payment para implementar com o resto do sistema

--- payments/models.py
+++ payments/models.py
@@ -1,9 +1,12 @@
 # Create your models here.
 from django.db import models
+from django.contrib.auth.models import User
+from Store.models import Order
 
 class Payment(models.Model):
-    seller_address = models.CharField(max_length=255)
-    initial_balance = models.FloatField()
+    order = model.OneToOneField(Order, on_delete=models.CASCADE, related_name="payment", null=True)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name="payments", null=True)
+    initial_balance = models.FloatField(default=0)
     amount_brl = models.FloatField()
     status = models.CharField(max_length=50, default='pending')
     created_at = models.DateTimeField(auto_now_add=True)
