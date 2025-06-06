55176d6b087d3d3a3ac358759508357327ed873b
ThiagoSiqueira <thiago.s.catharino@gmail.com>
2025-06-06 15:24:17 -0300

    Update urls.py

--- payments/urls.py
+++ payments/urls.py
@@ -1,8 +1,8 @@
 # payments/urls.py
 from django.urls import path
-from .views import CreatePaymentView, CheckPaymentView
+from .views import CheckPaymentView, PayView
 
 urlpatterns = [
-    path('pay/<uuid:order_id>/', CreatePaymentView.as_view(), name='payment'),
+    path('pay/<uuid:order_id>/', PayView.as_view(), name='pay'),
     path('check-payment/<int:payment_id>/', CheckPaymentView.as_view(), name='check-payment'),
 ]
