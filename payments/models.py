a437cdd10561cb927f90f74e5ce72b4066fc3f8a
ThiagoSiqueira <thiago.s.catharino@gmail.com>
2025-06-06 10:52:49 -0300

    Update models.py
    Retirada de código inútil, colocando initial_value

--- payments/models.py
+++ payments/models.py
@@ -2,13 +2,7 @@
 from django.db import models
 
 class Payment(models.Model):
-    address = models.CharField(max_length=255)
-    private_key = models.CharField(max_length=255)
-    #seller_address = models.CharField(max_length=255)
-    status = models.CharField(max_length=50, default='pending')  # novo campo
-    amount_received = models.DecimalField(max_digits=20, decimal_places=6, default=0)  # opcional
+    seller_address = models.CharField(max_length=255)
+    status = models.CharField(max_length=50, default='pending')
+    initial_balance = models.FloatField()# opcional
     created_at = models.DateTimeField(auto_now_add=True)
-
-    def transfer(self):
-        if self.status == "confirmed":
-            pass
