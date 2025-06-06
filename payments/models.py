eb92eb86bec70d8ce43db1dda353eae17284008d
Mauricio Ikeda <mauricioikeda17@gmail.com>
2025-06-06 15:16:46 -0300

    Update models.py

--- payments/models.py
+++ payments/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from Store.models import Order
 
 class Payment(models.Model):
-    order = model.OneToOneField(Order, on_delete=models.CASCADE, related_name="payment", null=True)
+    order = models.OneToOneField(Order, on_delete=models.CASCADE, related_name="payment", null=True)
     seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name="payments", null=True)
     initial_balance = models.FloatField(default=0)
     amount_brl = models.FloatField()
