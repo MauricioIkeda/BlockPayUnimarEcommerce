75b3e9346908943dbed5eb3b4a82d23dfedc3b29
ThiagoSiqueira <thiago.s.catharino@gmail.com>
2025-06-06 11:12:23 -0300

    Update models.py
    adicionei a quantia de Reais do pagamento.

--- payments/models.py
+++ payments/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Payment(models.Model):
     seller_address = models.CharField(max_length=255)
+    initial_balance = models.FloatField()
+    amount_brl = models.FloatField()
     status = models.CharField(max_length=50, default='pending')
-    initial_balance = models.FloatField()# opcional
     created_at = models.DateTimeField(auto_now_add=True)
